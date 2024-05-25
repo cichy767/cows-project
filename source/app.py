0c515103d511fa0d1b66c3b2b3f966b18a538517
cichy767 <vegitto767@gmail.com>
2024-05-25 15:57:40 +0200

    init

--- source/app.py
+++ source/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from database import init_db, db_session
 import os
 
 
@@ -8,4 +9,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
