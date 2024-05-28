fd474ac6dbcd0128655a593447276acf7cabcaa0
Mikolaj <vegitto767@gmail.com>
2024-05-28 15:48:44 +0200

    trying to debug gunicorn

--- cows_project/entrypoint.sh
+++ cows_project/entrypoint.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 # Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py runserver 0.0.0.0:8000
 #poetry run django-admin migrate --settings=cows_project.settings
 
 # Collect static files
 #poetry run django-admin collectstatic --noinput --settings=cows_project.settings
 
 # Start Gunicorn server
+#exec poetry run gunicorn cows_project.wsgi:application --bind 0.0.0.0:8000 --workers 3
+
+
+
+# Apply database migrations
+poetry run django-admin migrate --settings=cows_project.cows_project.settings
+
+# Collect static files
+#poetry run django-admin collectstatic --noinput --settings=cows_project.settings
+#
+## Start Gunicorn server with typeguard to enforce type hints
 #exec poetry run gunicorn cows_project.wsgi:application --bind 0.0.0.0:8000 --workers 3
