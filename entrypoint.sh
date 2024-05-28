ab4b86059da80dcd4a0781baebb17bffe1d4a6c1
Mikolaj <vegitto767@gmail.com>
2024-05-28 09:53:36 +0200

    fixing bug, but not finished

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 poetry run django-admin migrate --settings=cows_project.settings
 
 # Collect static files
-poetry run django-admin collectstatic --noinput --settings=cows_project.settings
+#poetry run django-admin collectstatic --noinput --settings=cows_project.settings
 
 # Start Gunicorn server
-exec poetry run gunicorn cows_project.wsgi:application --bind 0.0.0.0:8000 --workers 3
+#exec poetry run gunicorn cows_project.wsgi:application --bind 0.0.0.0:8000 --workers 3
