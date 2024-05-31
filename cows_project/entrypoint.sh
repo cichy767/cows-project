2a0fd79de27c8c5735f8f637f1fbff8737ea59e2
cichy767 <vegitto767@gmail.com>
2024-05-31 13:51:50 +0200

    polishing

--- cows_project/entrypoint.sh
+++ cows_project/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
-#chown -R $USER:$USERGROUP /app/static
-#$USER
 python manage.py makemigrations
 python manage.py migrate
 gunicorn -c gunicorn_config.py cows_project.wsgi:application
